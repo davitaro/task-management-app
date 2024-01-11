a86543f244150dbe6af71b3e378e5babcc2df1a5
DavitaR <92464615+DavitaR@users.noreply.github.com>
2024-01-11 09:11:05 -0500

    up to video 20

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    console.log('title', title);
+    console.log('description', description);
+    return this.tasksService.createTask(title, description);
+  }
+}
